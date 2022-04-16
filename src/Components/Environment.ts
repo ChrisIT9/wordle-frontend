6b71900744f8ac5e98a300207b632ab57def115d
ChrisIT9 <cri-9999@live.it>
2022-04-16 21:18:32 +0200

    changed socket endpoint

--- src/Components/Environment.ts
+++ src/Components/Environment.ts
@@ -1,2 +1,2 @@
 export const backendEndpoint = 'https://mordle-backend.herokuapp.com';
-export const socketEndpoint = 'https://mordle-backend.herokuapp.com:6969';
+export const socketEndpoint = 'https://mordle-backend.herokuapp.com';
